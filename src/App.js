414d63c78866980b0601dcc8b685c7146ea82207
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-15 19:00:25 +0530

    add Hero component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Hero from "./component/Hero";
 
 function App() {
   return (
     <div className="app">
-      <h1>welcome</h1>
+      <Hero />
     </div>
   );
 }
