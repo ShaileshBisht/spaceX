d621f6a083ee1fdc7f709450c7021bad9bec6011
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-15 18:42:57 +0530

    delete default style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>welcome</h1>
     </div>
   );
