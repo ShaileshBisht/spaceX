897fbe21cfdec223850b40385ffc00f8ead13138
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-16 23:13:04 +0530

    made about div lex to center its content

--- src/component/About.js
+++ src/component/About.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./About.css";
 
 function About() {
-  return <div className="about"></div>;
+  return (
+    <div className="about">
+      <div className="about_discription">
+        <p>
+          SpaceX is the innovative and ambitious private aerospace manufacturer
+          founded in 2002 by Elon Musk. But SpaceX truly earned its place among
+          the aeronautical elite—and changed the economics of space flight­—by
+          making its reusable rocket system seemingly as reliable as the
+          sunrise: Throughout the year, it landed eight rockets on ocean-based
+          drone platforms and sent three refurbished ones right back into the
+          skies.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
