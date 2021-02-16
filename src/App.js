cac68a0602ecc9e8617aed4a05dd93dd3f1aa0bf
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-16 15:21:09 +0530

    import Appbar component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Hero from "./component/Hero";
+import Appbar from "./component/Appbar";
 
 function App() {
   return (
     <div className="app">
+      <Appbar />
       <Hero />
     </div>
   );
