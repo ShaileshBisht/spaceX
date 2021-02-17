d3f9d216d181e49a2ed58ac69e42346e06a91095
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-17 20:31:42 +0530

    import Explore.css to Explore.js

--- src/component/Explore.js
+++ src/component/Explore.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Explore.css";
 
 function Explore() {
-  return <div className="explore"></div>;
+  return (
+    <div className="explore">
+      <h1>i am explore</h1>
+    </div>
+  );
 }
 
 export default Explore;
