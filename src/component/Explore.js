fd11c517dc0a1aeb135e29ad60208c0889141087
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-17 23:03:40 +0530

    change layout of Explore.js

--- src/component/Explore.js
+++ src/component/Explore.js
@@ -5,16 +5,18 @@ import ExploreCard from "./ExploreCard";
 function Explore() {
   return (
     <div className="explore">
-      <div className="explore_top">
-        <p>Explore</p>
-      </div>
-      <div className="explore_bottom">
-        <ExploreCard />
-        <ExploreCard />
-        <ExploreCard />
-        <ExploreCard />
-        <ExploreCard />
-        <ExploreCard />
+      <div className="explore_section">
+        <div className="explore_top">
+          <p>Explore</p>
+        </div>
+        <div className="explore_bottom">
+          <ExploreCard />
+          <ExploreCard />
+          <ExploreCard />
+          <ExploreCard />
+          <ExploreCard />
+          <ExploreCard />
+        </div>
       </div>
     </div>
   );
