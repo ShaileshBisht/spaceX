5a30ee525e4b595fe7fef763b452b35f4775240e
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-17 21:47:11 +0530

    add title and discription

--- src/component/ExploreCard.js
+++ src/component/ExploreCard.js
@@ -2,7 +2,20 @@ import React from "react";
 import "./ExploreCard.css";
 
 function ExploreCard() {
-  return <div className="exploreCard"></div>;
+  return (
+    <div className="exploreCard">
+      <img src="" alt="" />
+      <div className="ExploreCard_discription">
+        <h1>Mission</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam beatae
+          eius veritatis corporis architecto quas quae expedita ipsa iusto
+          voluptatum repellendus, dignissimos hic harum aperiam illum suscipit
+          similique ipsum quis?
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default ExploreCard;
