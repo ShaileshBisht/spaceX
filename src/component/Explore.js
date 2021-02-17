2ab136697a6eddf8ed5c4a5b0e6c70a3d41274ca
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-17 23:42:26 +0530

    on clicking back button in Explore.js take user to home screen

--- src/component/Explore.js
+++ src/component/Explore.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import "./Explore.css";
 import ExploreCard from "./ExploreCard";
 
 function Explore() {
+  const history = useHistory();
+
   return (
     <div className="explore">
       <div className="explore_section">
         <div className="explore_top">
           <p>Explore</p>
-          <button className="btn explore-back">back</button>
+          <button
+            onClick={() => history.push("/")}
+            className="btn explore-back"
+          >
+            back
+          </button>
         </div>
         <div className="explore_bottom">
           <ExploreCard />
