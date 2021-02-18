f6700011c1816cfe3c5302d52e9a9fe2286c79a9
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-18 10:58:41 +0530

    add first cardDetails

---
+++ src/ExploreCardDetails.js
@@ -0,0 +1,14 @@
+import img1 from "./assets/images/explore3.jpg";
+import img2 from "./assets/images/explore4.jpg";
+import img3 from "./assets/images/explore5.jpg";
+import img4 from "./assets/images/explore6.jpg";
+import img5 from "./assets/images/explore7.jpg";
+
+export const cardDetails = [
+  {
+    id: 1,
+    class: "card-1",
+    img: `${img1}`,
+    title: "History",
+  },
+];
